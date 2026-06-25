5e161bb281fd694843b99321f9712bd1a7f7c6d7
RadioSourcesdeVie <avenangar@gmail.com>
2026-06-25 08:47:01 -0400

    Restauration run_news.bat propre + nettoyage debug

--- scripts/run_news.bat
+++ scripts/run_news.bat
@@ -1,6 +1,3 @@
 @echo off
 cd /d "%~dp0.."
-echo DOSSIER ACTUEL: %CD% > task_debug.log
-echo --- >> task_debug.log
-"C:\Program Files\Git\bin\bash.exe" scripts/run_news.sh >> task_debug.log 2>&1
-echo FIN CODE: %ERRORLEVEL% >> task_debug.log
+"C:\Program Files\Git\bin\bash.exe" scripts/run_news.sh
